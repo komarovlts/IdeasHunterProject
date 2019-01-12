e987637c439e58072836bcf11823ca9d84733da0
Omar Carrasco <omar.carrasco@usach.cl>
2019-01-12 04:10:00 -0300

    Archivos faltantes

---
+++ src/main/resources/static/precache-manifest.47b3ee643ca9a02830efd92c08679224.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "96eb1cfa538d0095ba17",
+    "url": "/static/css/main.b850bca5.chunk.css"
+  },
+  {
+    "revision": "96eb1cfa538d0095ba17",
+    "url": "/static/js/main.96eb1cfa.chunk.js"
+  },
+  {
+    "revision": "78735dec0abb885d7125",
+    "url": "/static/css/1.c47d1fa5.chunk.css"
+  },
+  {
+    "revision": "78735dec0abb885d7125",
+    "url": "/static/js/1.78735dec.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a6eb615e767e31d140b94ed8b622e8bb",
+    "url": "/index.html"
+  }
+];
