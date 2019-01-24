05adaba77d08b77df388b51fd62ed2a5114697fe
Jose <jose.maureira.b@usach.cl>
2019-01-24 01:03:36 -0300

    Aquí muere el proyecto

---
+++ src/main/resources/static/precache-manifest.42f072082e8686f8afad1b0bbb8a2e68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bf4f24526d52b6f6ef0",
+    "url": "/static/css/main.e8d9e80a.chunk.css"
+  },
+  {
+    "revision": "9bf4f24526d52b6f6ef0",
+    "url": "/static/js/main.9bf4f245.chunk.js"
+  },
+  {
+    "revision": "96874f58f09a763c8b0f",
+    "url": "/static/css/1.b638a016.chunk.css"
+  },
+  {
+    "revision": "96874f58f09a763c8b0f",
+    "url": "/static/js/1.96874f58.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fdaf557b27e1b160e1580be9df1342a7",
+    "url": "/index.html"
+  }
+];
